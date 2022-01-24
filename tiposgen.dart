0b2c362c2eba371a9b371f15f61c3f95f9a806a5
rafacampos10 <rafinhasep94@hotmail.com>
2022-01-24 09:45:22 -0300

    tipos genéricos

--- tiposgen.dart
+++ tiposgen.dart
@@ -1,22 +1,38 @@
 //tipos genéricos
 
-class Tuple{
-  final int? _a;
-  final int? _b;
-  final int? _c;
+class Tuple<E>{
+  final E? _a;
+  final E? _b;
+  final E? _c;
 
-  int? get first => _a;
-  int? get second => _b;
-  int? get third => _c;
+  E? get first => _a;
+  E? get second => _b;
+  E? get third => _c;
 
   const Tuple(this._a,this._b,this._c);
-  Tuple.fromList(List<int> list)
+  Tuple.fromList(List<E> list)
   : _a = list.asMap().containsKey(0) ? list[0]:null,
         _b = list.asMap().containsKey(1) ? list[1]:null,
         _c = list.asMap().containsKey(2) ? list[2]:null ;
 
-  Tuple operator + (Tuple t) => Tuple(_a! + t._a!,_b! + t._b!, _c! + t._c!);
-  Tuple operator - (Tuple t) => Tuple(_a! - t._a!, _b! - t._b!, _c! - t._c!);
+Tuple<num> operator + (Tuple<num> t){
+if(this is Tuple<num>){
+  final thisAsTupleNum = this as Tuple <num>;
+return Tuple(thisAsTupleNum._a! + t._a!, thisAsTupleNum._b! + t._b!,
+thisAsTupleNum._c! + t._c!);
+}
+return const Tuple(0, 0, 0);
+}
+
+  Tuple<num> operator - (Tuple<num> t){
+    if(this is Tuple<num>){
+      final thisAsTupleNum = this as Tuple <num>;
+      return Tuple(thisAsTupleNum._a! - t._a!, thisAsTupleNum._b! - t._b!,
+          thisAsTupleNum._c! - t._c!);
+    }
+    return const Tuple(0, 0, 0);
+  }
+
 
   @override
   String toString() {
@@ -25,14 +41,11 @@ class Tuple{
   }
 
 
-void main(){
-const Tuple tuple1 = Tuple(1, 2, 3);
-final Tuple tuple2 = Tuple.fromList([4, 5, 6]);
-final Tuple tuple3 = Tuple.fromList([7]);
-final Tuple tuple4 = tuple1 + tuple2;
-
-print("tuple1 --> $tuple1 ");
-print("tuple2 --> $tuple2 ");
-print("tuple3 --> $tuple3 ");
-print("tuple4 --> $tuple4 ");
+void main(List<String> arguments){
+const t1 = Tuple(1, 2, 3);
+const t2 = Tuple(4, 5, 6);
+final tSum = t1+t2;
+final tDif = t1 - t2;
+print(tSum);
+print(tDif);
 }
